e3462c1e6da32304363745495a420d3cd0869b18
Max Woolf <maximus@zingysaturn.co.uk>
2013-07-09 20:03:34 +0100

    add list view controller

---
+++ what/WHQuestionListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WHQuestionListViewController.h
+//  what
+//
+//  Created by Max Woolf on 09/07/2013.
+//  Copyright (c) 2013 maxwoolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHQuestionListViewController : UIViewController
+
+@end
---
+++ what/WHQuestionListViewController.m
@@ -0,0 +1,38 @@
+//
+//  WHQuestionListViewController.m
+//  what
+//
+//  Created by Max Woolf on 09/07/2013.
+//  Copyright (c) 2013 maxwoolf. All rights reserved.
+//
+
+#import "WHQuestionListViewController.h"
+
+@interface WHQuestionListViewController ()
+
+@end
+
+@implementation WHQuestionListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
